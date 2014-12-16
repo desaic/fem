b62c39daf5e517f9490037929772772b9b4e749f
desaic <desaicwtf@gmail.com>
2014-12-16 15:08:07 -0500

    C in formulas

--- runtime/runtime.cpp
+++ runtime/runtime.cpp
@@ -84,9 +84,9 @@ int main(int argc, char* argv[])
   //StepperNewton *stepper= new StepperNewton();
   //stepper->rmRigid = true;
   //IpoptStepper * stepper = new IpoptStepper();
-  AdmmCPU * stepper = new AdmmCPU();
-  stepper->ro0 = 500;
-  //AdmmNoSpring *stepper = new AdmmNoSpring();
+  //AdmmCPU * stepper = new AdmmCPU();
+  //stepper->ro0 = 500;
+  AdmmNoSpring *stepper = new AdmmNoSpring();
   stepper->nSteps = 1000;
   std::thread simt(runSim, em, stepper);
   Render render;
