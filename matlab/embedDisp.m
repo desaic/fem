192537d35de87498d6b5d72e66619bca850c806b
desaic <desaicwtf@gmail.com>
2015-02-12 18:02:07 -0500

    exp matlab

--- matlab/embedDisp.m
+++ matlab/embedDisp.m
@@ -4,4 +4,23 @@
 function u=embedDisp(p, qf, qc, pf, Xf, Xc)
   %compute fine displacement
   Nf=bilinear(p);
+  uf=zeros(1,2);
+  for ii = 1:4
+    uf = uf + Nf(ii)*qf(ii,:);
+  end
+  %point in reference frame
+  Xp=zeros(1,2);
+  for ii = 1:4
+    Xp = Xp + Nf(ii) * Xf(ii,:);
+  end
+  %natural coordinate in coarse element
+  pc = natCoord(Xp,Xc);
+  Fc = defGrad(pc,qc,Xc);
+  Nc = bilinear(pc);
+  %coarse displacement
+  uc = zeros(1,2);
+  for ii = 1:4
+      uc = uc + Nc(ii)*qc(ii,:);
+  end
+  u = uc + Fc*uf;
 end
