16814be2bd6ce43a5d2e6d94e63ab6b1c39fdbdc
Desai <desaicwtf@gmail.com>
2015-12-11 18:01:37 -0500

    adding cont opt

--- MaterialOpt/main.cpp
+++ MaterialOpt/main.cpp
@@ -1,8 +1,11 @@
 #include "assert.h"
 #include "cfgUtilities.h"
 #include "ConfigFile.hpp"
+#include "FileUtil.hpp"
+
 #include <cmath>
 #include <iostream>
+
 int main(int argc, char * argv[]){
   const char * filename = "config.txt";
   if(argc>1){
@@ -11,6 +14,11 @@ int main(int argc, char * argv[]){
   ConfigFile conf;
   conf.load(filename);
 
+  return 0;
+}
+
+void binaryToText(ConfigFile & conf)
+{
   std::string fileRootName("../data/Level16");
   std::string fileExtension(".bin");
   if(conf.hasOpt("fileRootName")){
@@ -22,37 +30,39 @@ int main(int argc, char * argv[]){
   std::vector<std::vector<int> > materialAssignments;
   std::vector<std::vector<float> > tensors;
   bool success ;
-  std::string matAssignmentFile = fileRootName + "_matAssignments" + fileExtension;
-  std::string tensorFile = fileRootName + "_elasticityTensors" + fileExtension;
+  std::string matAssignmentFile = "../data/level16_SMC_matAssignments.bin";//fileRootName + "_matAssignments" + fileExtension;
+
   success = cfgUtil::readBinary<int>(matAssignmentFile, materialAssignments);
   if(!success){
     std::cout<<"Can't read "<< matAssignmentFile<<"\n";
   }
-  success = cfgUtil::readBinary<float>( tensorFile, tensors);
-  if(!success){
-    std::cout<<"Can't read "<< matAssignmentFile<<"\n";
-  }
+
+  FileUtilOut out("microstructure.txt");
+
+  int len = (int)materialAssignments[0].size();
+  std::cout<< len <<"\n";
+  out.out<<materialAssignments.size()<<" "<<len<<"\n";
   for(unsigned int ii = 0;ii<materialAssignments.size(); ii++){
-    int len = (int)materialAssignments[ii].size();
-    std::cout<< len <<"\n";
-    int nx = std::sqrt(len);
     for(int jj = 0; jj<len; jj++){
-      int xi = jj/nx;
-      int yi = jj%nx;
-      int pxi = yi;
-      int pyi = nx-xi-1;
-      std::cout<<materialAssignments[ii][pxi*nx+pyi]<<" ";
-      if(pxi == nx-1){
-        std::cout<<"\n";
-      }
+      out.out<<materialAssignments[ii][jj]<<" ";
     }
-
-    len = (int)tensors[ii].size();
-    for(int jj = 0; jj<len; jj++){
-      std::cout<<tensors[ii][jj]<<" ";
-    }
-    std::cout<<"\n";
+    out.out<<"\n";
   }
 
-  return 0;
+  //load and save tensors in plain text
+  //  std::string tensorFile = fileRootName + "_elasticityTensors" + fileExtension;
+  //  success = cfgUtil::readBinary<float>( tensorFile, tensors);
+  //  if(!success){
+  //    std::cout<<"Can't read "<< tensorFile<<"\n";
+  //  }
+  //  FileUtilOut tensorOut("elasticity.txt");
+  //  int tensorLen = (int)tensors[0].size();
+  //  tensorOut.out<<tensors.size()<<" "<<tensorLen<<"\n";
+  //  for(unsigned int ii = 0;ii<materialAssignments.size(); ii++){
+  //    for(int jj = 0; jj<tensorLen; jj++){
+  //      std::cout<<tensors[ii][jj]<<" ";
+  //      tensorOut.out<<tensors[ii][jj]<<" ";
+  //    }
+  //    tensorOut.out<<"\n";
+  //  }
 }
